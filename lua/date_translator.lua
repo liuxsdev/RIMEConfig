e1aa42767f41250ec906e0b36f9cc54e666ecf28
liuxsdev <liuxspro@gmail.com>
2023-08-18 20:01:47 +0800

    update date lua

--- lua/date_translator.lua
+++ lua/date_translator.lua
@@ -1,9 +1,15 @@
 local function translator(input, seg)
+    -- https://github.com/Mintimate/oh-my-rime/blob/main/Lua/date.lua
+    local function create_candidate(date_str, comment)
+        -- local candidate = Candidate(type, start, end, text, comment)
+        local candidate = Candidate("date", seg.start, seg._end, date_str, comment)
+        candidate.quality = 100
+        yield(candidate)
+    end
     if (input == "date" or input == "riqi") then
-       --- Candidate(type, start, end, text, comment)
-       yield(Candidate("date", seg.start, seg._end, os.date("%Y年%m月%d日"), " 📅️"))
-       yield(Candidate("date", seg.start, seg._end, os.date("%Y-%m-%d"), " 📅️"))
+        create_candidate(os.date("%Y年%m月%d日"),'📅️')
+        create_candidate(os.date("%Y-%m-%d"),'📅️')
     end
 end
- 
+
 return translator
